295b0812b7ac259402915df2db296a5d64475519
Samioul51 <akmsamioulislam51@gmail.com>
2026-01-02 14:37:36 +0600

    Dark mode feature enhanced

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,19 +7,19 @@ import toast, { Toaster } from 'react-hot-toast';
 const Navbar = () => {
     const { user, logout } = use(AuthContext);
 
-    const [theme,setTheme]=useState(localStorage.getItem("theme")|| "light");
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
-    useEffect(()=>{
-        const html=document.querySelector("html");
-        html.setAttribute("data-theme",theme);
-        localStorage.setItem("theme",theme)
-    },[theme])
+    useEffect(() => {
+        const html = document.querySelector("html");
+        html.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme)
+    }, [theme])
 
     const handleOpenModal = () => document.getElementById("my_modal_5").showModal();
     const handleCloseModal = () => document.getElementById("my_modal_5").close();
 
-    const handleTheme=(checked)=>{
-        setTheme(checked?"dark":"light");
+    const handleTheme = (checked) => {
+        setTheme(checked ? "dark" : "light");
     }
 
     const handleLogout = () => {
@@ -58,11 +58,37 @@ const Navbar = () => {
                 }
             </div>
             <div className='flex flex-col items-center lg:flex-row gap-[30px]'>
-                <input
-                    onChange={(e) => handleTheme(e.target.checked)}
-                    type="checkbox"
-                    defaultChecked={localStorage.getItem('theme') === "dark"}
-                    className="toggle" />
+                <label className="flex cursor-pointer gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <circle cx="12" cy="12" r="5" />
+                        <path
+                            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                    </svg>
+                    <input onChange={(e) => handleTheme(e.target.checked)}
+                        type="checkbox"
+                        defaultChecked={localStorage.getItem('theme') === "dark"} className="toggle theme-controller" />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round">
+                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                    </svg>
+                </label>
                 {
                     user ? (
                         <>
